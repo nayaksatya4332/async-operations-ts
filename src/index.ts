6333a6e48325a77bb0cfbf6957e22e5f50e0d5dc
nayaksatya4332@gmail.com <nayaksatya4332@gmail.com>
2024-10-13 18:49:21 +0530

    Callbacks replaced with Promise

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { getUserCallback } from "./services/github/callbacks";
-import { getUser } from "./services/github/functions";
+import { getCommits, getRepositories, getUser } from "./services/github/functions";
 
-getUser(100, getUserCallback);
+getUser(100)
+    .then((user) => getRepositories(user.email))
+    .then((repos) => getCommits(repos[0].name))
+    .then((commits) => console.log(commits))
+    .catch((error) => console.log(error.message));
--- src/services/github/functions.ts
+++ src/services/github/functions.ts
@@ -1,22 +1,29 @@
 import { User, Repository, Commit } from './types';
 
-export function getUser(id: number, callback: (arg: User) => void):void {
+export function getUser(id: number): Promise<User> {
     console.log('Fetching user Details..');
-    setTimeout(() => {
-        callback({ id: 100, email: 'satya@gmail.com' });
-    }, 2000);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({ id: 100, email: 'satya@gmail.com' });
+        }, 2000);
+    });
 }
 
-export function getRepositories(email: string, callback: (args: Repository[]) => void):void {
+export function getRepositories(email: string): Promise<Repository[]> {
     console.log("Fetching Repositories of the User..")
-    setTimeout(() => {
-        callback([{ name: 'repository 01' }, { name: 'repository 02' }, { name: 'repository 03' }]);
-    }, 2000);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([{ name: 'repository 01' }, { name: 'repository 02' }, { name: 'repository 03' }]);
+        }, 2000);
+    });
 }
 
-export function getCommits(repositoryName: string, callback: (commits: Commit[]) => void):void {
+export function getCommits(repositoryName: string): Promise<Commit[]> {
     console.log("fetching Commit for the Repository..");
-    setTimeout(() => {
-        callback([{ commitId: 'commit 01', time: new Date().toISOString() }, { commitId: 'commit 02', time: new Date().toISOString() }]);
-    }, 2000);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([{ commitId: 'commit 01', time: new Date().toISOString() }, { commitId: 'commit 02', time: new Date().toISOString() }]);
+        }, 2000);
+    });
+
 }
